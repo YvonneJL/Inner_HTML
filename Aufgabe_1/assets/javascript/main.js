9414a9ec73ef5fd912e61bef51e685cd3d5ba3d1
YvonneJL <yvonneloder@gmx.de>
2024-12-10 14:39:33 +0100

    fertig

---
+++ Aufgabe_1/assets/javascript/main.js
@@ -0,0 +1,17 @@
+const newH1 = document.getElementById("info");
+// erst einmal Element mit variablen definieren
+
+newH1.innerHTML = "<h1>Hello World</h1>";
+
+newH1.innerHTML += "<h2>How are you?</h2>";
+// hier füge ich h1 ein und h2 hinzu (+=)
+
+
+
+
+const newPTag = document.getElementById("container");
+
+newPTag.innerHTML = "<p>start of the element</p>";
+
+
+document.write("<p>end of the element</p>");
---
+++ Aufgabe_2/assets/js/main.js
@@ -0,0 +1,6 @@
+let newFigureTag = document.getElementById("gallery");
+
+
+newFigureTag.innerHTML = "<figure><img src='https://images.unsplash.com/photo-1731612941949-d4da051f7ea8?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='random'><figcaption>Fig. 1</figcaption></figure>";
+newFigureTag.innerHTML += "<figure><img src='https://images.unsplash.com/photo-1733667917418-f4b7ea5a80c4?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='random'><figcaption>Fig. 2</figcaption></figure>";
+newFigureTag.innerHTML += "<figure><img src='https://images.unsplash.com/photo-1732282537693-77dcd910a172?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='random'><figcaption>Fig. 3</figcaption></figure>";
